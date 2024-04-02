27a337d50d1ed8015dd586d4c4945bdb346d8a34
estebandurangov <esteban.durangov@udea.edu.co>
2024-04-02 12:20:19 -0500

    Add estructura de Propuesta, whitelist, lista de votantes y constructor

---
+++ Votos.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract VotingSystem {
+
+    //Una propuesta consta de un nombre (nombre de la propuesta) y un contador de votos (que ha recibido la propuesta).
+
+    struct Propuesta {
+        string nombre;
+        uint votos;
+    }
+    
+    //lista de propuesta
+    Propuesta[] public propuestas;
+
+    //direccion Ethereum del administrador
+    address public admin;
+    
+    //almacenar direcciones de votantes habilitados y de quienes han votado
+    mapping(address => bool) public whitelist;
+    mapping(address => bool) public hasVoted;
+
+    uint public endVoteTime;
+
+    constructor() {
+        admin = msg.sender;
+        endVoteTime = block.timestamp + 3 days;
+    }
+}
